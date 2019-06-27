5cdb513831254bb5c568c1724cb201c6ff28e7b3
Violet <Violet@DESKTOP-J9KSVUM>
2019-06-27 18:49:43 +0800

    spring cloud
    Signed-off-by: AokoSeven <50785969+AokoSeven@users.noreply.github.com>

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,18 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	//商品编号
+	private Integer id;
+	//商品名称
+	private String name;
+	//商品数量
+	private Integer number;
+	
+}
